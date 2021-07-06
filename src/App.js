e8daabae2507671f1969dc297085cfbdb254344a
Dalin Oluoch <30606041+dcDalin@users.noreply.github.com>
2021-07-06 14:13:06 +0300

    Update our app header

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Our Cool Cat Application
         </a>
       </header>
     </div>
